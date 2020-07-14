b2a7f36fcf4b88012dafc49ba9c2886ffc9d5349
Angelo Moquera <anggelomos@outlook.com>
2020-07-14 12:42:45 -0500

    Add README content as the long description in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
 setup(
     name="whatsapp-wordlcloud-generator",
     version="1.1.0",
     description="Creates a wordcloud with the most used words in a whatsapp conversation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Angelo Mosquera",
     license="MIT",
     keywords="whatsapp wordcloud generator",
