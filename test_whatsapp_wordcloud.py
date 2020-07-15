be31c6ac16912dabbc4aec5779f867d4dd98b0a6
Angelo Mosquera <anggelomos@outlook.com>
2020-07-15 12:26:29 -0500

    Add test for contact_text_separator()

--- test_whatsapp_wordcloud.py
+++ test_whatsapp_wordcloud.py
@@ -3,6 +3,7 @@
 import unittest
 from whatsapp_wordcloud import _punctuation_cleaner
 from whatsapp_wordcloud import text_cleaner
+from  whatsapp_wordcloud import contact_text_separator
 
 class TestWhatsappWordcloud(unittest.TestCase):
 
@@ -15,6 +16,15 @@ class TestWhatsappWordcloud(unittest.TestCase):
         testcase = "Hola, como está usted? -se siente bien."
         expected = ["hola", "como", "está", "usted", "se", "siente", "bien"]
         self.assertEqual(text_cleaner(testcase), expected)
+
+    def test_contact_text_separtor(self):
+        self.maxDiff = None
+
+        testcase = "test files\\testfile_01.txt"
+        expected = {'contacto 1': "Hola, ¿alguien sabe por qué el sol sale por las mañanas? Interesante, muchas gracias, estuve mucho tiempo buscando la respuesta ",
+                    'contacto 2': "Saludos, la respuesta es muy sencilla: Porque hay un momento  del día en el que tiene que ser por la mañana, entonces le toca salir, para ver más información puede entrar a https://elsolsaliohoy.? No hay problema . ",
+                    'contacto 3': "Eso es: falso "}
+        self.assertEqual(contact_text_separator(testcase), expected)
 
 if __name__ == "__main__":
     unittest.main()
