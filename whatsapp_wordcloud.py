4158b4f8d4d2fbd3c03cd6141f1d505e2db30125
Angelo Mosquera <anggelomos@outlook.com>
2020-07-16 07:20:10 -0500

    Add the function file_finder() to the main script

--- whatsapp_wordcloud.py
+++ whatsapp_wordcloud.py
@@ -20,6 +20,7 @@ import re
 import os
 import random
 import collections
+from typing import Union
 
 
 class GroupedColorFunc(object):
@@ -175,7 +176,29 @@ def color_generator(contacts: dict) -> list:
 
     return color_pallette
 
+def file_finder(directory: str, file_extension: Union[str, tuple] = "") -> list:
+    """Generate a list containing both the name and path of each file with the specified extension in the directory.
 
+    Parameters
+    ----------
+    directory : str
+        Directory where you want to get the files.
+    file_extension : Union[str, tuple], optional
+        The file extension or extensions you want to include in your list, by default "" (includes all files)
+
+    Returns
+    -------
+    file_names: list
+        List containing both the name and the path of each file in the specified directory.
+    """
+
+    file_names = []
+    for file_name in os.listdir(directory):
+        if file_name.endswith(file_extension):
+            file_path = os.path.join(directory, file_name)
+            file_names.append([file_name, file_path])
+
+    return file_names
 
 """
 
